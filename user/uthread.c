093768400b98cbc9083c2ee09bdcf7ea62b55e63
Hwee Ren <hr@hweeren.com>
2026-03-18 18:48:57 +0800

    user threading library

--- user/uthread.c
+++ user/uthread.c
@@ -11,16 +11,34 @@
 #define MAX_THREAD  4
 
 
+struct context {
+  uint64 ra;      // Return address (x1)
+  uint64 sp;      // Stack pointer (x2)
+  uint64 s0;      // Saved registers / Frame pointer (x8)
+  uint64 s1;      // (x9)
+  uint64 s2;      // (x18)
+  uint64 s3;      // (x19)
+  uint64 s4;      // (x20)
+  uint64 s5;      // (x21)
+  uint64 s6;      // (x22)
+  uint64 s7;      // (x23)
+  uint64 s8;      // (x24)
+  uint64 s9;      // (x25)
+  uint64 s10;     // (x26)
+  uint64 s11;     // (x27)
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
   // TODO: include context of thread
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
-void 
+
+void
 thread_init(void)
 {
   // main() is thread 0, which will make the first invocation to
@@ -30,7 +48,7 @@ thread_init(void)
   current_thread->state = RUNNING;
 }
 
-void 
+void
 thread_schedule(void)
 {
   struct thread *t, *next_thread;
@@ -59,11 +77,12 @@ thread_schedule(void)
     current_thread = next_thread;
     // TODO: invoke thread_switch to switch from t to next_thread:
     // thread_switch(??, ??);
+    thread_switch(&t->context, &current_thread->context);
   } else
     next_thread = 0;
 }
 
-void 
+void
 thread_create(void (*func)())
 {
   struct thread *t;
@@ -71,12 +90,26 @@ thread_create(void (*func)())
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
+  if (t >= all_thread + MAX_THREAD) {
+    printf("thread_create: no free threads\n");
+    exit(-1);
+  }
+
   t->state = RUNNABLE;
+
   // TODO: ensure `func` will be executed on its own stack
   // ...
+  // Initialize context: zero out all saved registers
+  memset(&t->context, 0, sizeof(t->context));
+
+  // Set RA to the thread function so thread_switch will "return" to it
+  t->context.ra = (uint64)func;
+
+  // Set SP to the top of the stack (highest address)
+  t->context.sp = (uint64)(t->stack + STACK_SIZE);
 }
 
-void 
+void
 thread_yield(void)
 {
   current_thread->state = RUNNABLE;
